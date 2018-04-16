2a06b1fe0b20c4e8554ce706a42f75ceb02892d8
lamarmeigs <lmeigs@shiftgig.com>
2018-04-16 15:24:32 -0500

    Register command with awscli

--- awscli_console_login/__init__.py
+++ awscli_console_login/__init__.py
@@ -1,1 +1,9 @@
-# TODO: define awscli_initialize
+from .login import ConsoleLogin
+
+
+def awscli_initialize(cli):
+    cli.register('building-command-table.main', inject_commands)
+
+
+def inject_commands(command_table, session, **kwargs):
+    command_table['console-login'] = ConsoleLogin(session)
