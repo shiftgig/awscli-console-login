4f91730d381a112aa330746a6419248047c484a2
lamarmeigs <lmeigs@shiftgig.com>
2018-04-18 12:32:40 -0500

    Formalize setup.py for release

--- setup.py
+++ setup.py
@@ -4,18 +4,23 @@ from setuptools import setup, find_packages
 def read_readme():
     """Attempt to read some README file"""
     try:
-        return open('README.rst').read()
-    except IOError:
-        return open('README.md').read()
+        import pypandoc
+        long_description = pypandoc.convert_file('README.md', 'rst')
+    except ImportError:
+        long_description = open('README.md').read()
+    return long_description
 
 
 setup(
     name='awscli-console-login',
     version='0.0.1',
-    # url='',
+    url='https://github.com/shiftgig/awscli-console-login',
     author='Lamar Meigs, Jacob Dougherty, Shiftgig, Inc',
     author_email='lmeigs@shiftgig.com, jacob@shiftgig.com',
-    description='An aws-cli plugin to login to an account via the browser',
+    description=(
+        'An AWS CLI plugin to convert configured roles into active AWS '
+        'Console sessions'
+    ),
     long_description=read_readme(),
     packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=['awscli', 'requests'],
@@ -28,12 +33,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: System :: Distributed Computing',
     ]
